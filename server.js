46be9823b11050970e8f89ffac2bd2dd3affaec3
Shantanu Kadu <shntnkd@gmail.com>
2020-01-02 12:28:24 +0530

    Initialie Repo

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require ('express');
+var app = express();
+var port = 3000;
+
+var middleWare={
+	requireAuthentication: function(req,res,next) {
+		console.log('private Route hit');
+
+	},
+	logger: function(req,res,next) {
+		console.log('Request'+ new Date().toString()+' '+req.method+' '+req.originalUrl);
+		next();
+	}
+};
+app.use(middleWare.logger);
+
+app.get('/about',app.use,middleWare.requireAuthentication,function(req,res) {
+	res.send("About Us");
+})
+app.use(express.static(__dirname + '/public'));
+app.listen(port,function() {
+	console.log("express server started on port "+ port +"!!");
+});
